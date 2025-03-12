e4eb1f25a0b73fc4e9dbdccdb62b91777f25b303
meharkp7 <meharkapoor7@gmail.com>
2025-03-12 12:27:34 +0530

    Add files via upload

---
+++ masterclass.js
@@ -0,0 +1,11 @@
+document.getElementById("take-assessment").addEventListener("click", function () {
+    alert("Starting Assessment Test...");
+});
+
+document.getElementById("attempt-now").addEventListener("click", function () {
+    alert("Attempting Now...");
+});
+
+document.getElementById("post-assessment").addEventListener("click", function () {
+    alert("Starting Post Class Assessment...");
+});
